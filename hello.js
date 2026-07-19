44381f92b36e4a484da34772d5b89f10565c05e6
reyan-ahmad <abdullahnaseem002@gmail.com>
2026-07-20 01:38:47 +0530

    add greet helper

---
+++ hello.js
@@ -0,0 +1,8 @@
+// Say hi to a user by name.
+function greet(name) {
+  const message = "Hello, " + name + "!";
+  document.getElementById('greeting').innerHTML = message;
+  return message;
+}
+
+module.exports = { greet };
